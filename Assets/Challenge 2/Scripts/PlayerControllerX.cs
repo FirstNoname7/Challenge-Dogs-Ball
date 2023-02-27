646de7ed483f1d0cc2a34e52ec33963c732e4e07
Maria20Maria20 <apelnjam@gmail.com>
2023-02-27 15:28:46 +0400

    2 version
    Добавлена задержка между спауном собак, чтобы нельзя было флудить ими

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float maxTime = 3; //максимальное время таймера
+    public float timer = 3.0f; //таймер
+    public bool dogSpawn = true; //проверка, можно ли в определённый момент спаунить объект (собак) или нет
 
+
+    // Update is called once per frame
     void Update()
     {
-        // Создаем собаку, нажимая на пробел
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogSpawn) //если игрок нажал на пробел и (&&) если dogSpawn = true, то:
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation); //запускаем собаку
+            dogSpawn = false; //и ставим dogSpawn на false (чтобы в эту же секунду нельзя было выпустить новую собаку)
+        }
+
+        if (timer > 0) //если таймер больше нуля, то:
+        {
+            timer -= Time.deltaTime; //уменьшаем время
+        }
+        else //в обратном случае (то есть если таймер = 0), то:
+        {
+            dogSpawn = true; //возобновляем спаун собак
+            timer = maxTime; //и возобновляем таймер (делаем его максимальным, ибо он равен 0)
         }
     }
 }
