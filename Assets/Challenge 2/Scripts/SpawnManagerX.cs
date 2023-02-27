c58df9a44f268f1793814160278c7007e322c1be
Maria20Maria20 <apelnjam@gmail.com>
2023-02-27 10:20:34 +0400

    1 version

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 25;
+    private Vector3 spawnPos;
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Спавн случайного шара на рандомной позиции
+    void SpawnRandomBall ()
+    {
+        // Генерация рандомного индекса и случайной позиции 
+        spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int randPos = Random.Range(0, ballPrefabs.Length);
+
+        // Создание шара
+        Instantiate(ballPrefabs[randPos], spawnPos, ballPrefabs[randPos].transform.rotation);
+    }
+
+}
